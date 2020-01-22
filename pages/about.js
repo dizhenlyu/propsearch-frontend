60a86e467123f8189f81e76d436dc9fcef556234
dizhenlyu <dizhenlyu@gmail.com>
2020-01-22 14:31:07 -0800

    material ui setup

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import MuiLink from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 
@@ -19,16 +20,16 @@ function Copyright() {
   );
 }
 
-export default function Index() {
+export default function About() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <Button variant="contained" color="primary" component={Link} naked href="/">
+          Go to the main page
+        </Button>
         <ProTip />
         <Copyright />
       </Box>
