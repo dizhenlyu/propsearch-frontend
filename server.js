a4ed59a5b8d44365f40e33524168bb63282bb2b3
dizhenlyu <dizhenlyu@gmail.com>
2020-01-29 22:16:57 -0800

    confirmed homefacts can use crawler

--- server.js
+++ server.js
@@ -12,10 +12,18 @@ app.prepare()
 		const server = express()
 
 		server.get('/test', (req, res) => {
-      scrapeIt('https://www.homes.com/property/19540-pulling-st-south-bend-in-46614/id-600034304784/', {
-        price: ".home-estimate > .price"
+    //   scrapeIt('https://www.homes.com/property/19540-pulling-st-south-bend-in-46614/id-600034304784/', {
+    //     price: ".home-estimate > .price"
+    // }).then(({ data, response }) => {
+    //     console.log(`Status Code: ${response.statusCode}`)
+    //     console.log(data)
+    //     res.json(data);
+    // })
+    scrapeIt('https://www.homefacts.com/address/Indiana/St.-Joseph-County/South-Bend/46614/19540-Pulling-St/212333813.html', {
+      price: "#forsale-listing-price"
     }).then(({ data, response }) => {
-        console.log(`Status Code: ${response.statusCode}`)
+      console.log(response)
+        console.log(response.statusCode)
         console.log(data)
         res.json(data);
     })
